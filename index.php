ad9f0b732f7e06d1bd02a72aaacac45c3fe2ad0b
lenochware <lenochware@gmail.com>
2018-06-08 09:58:36 +0200

    removed .gitattributes

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $app = new App('ftpdeploy');
 $app->addConfig('./config.php');
 //$app->debugMode = $app->config['padmin.debugmode'];
 $app->setLayout($_GET['popup']? 'tpl/popup.tpl' : 'tpl/website.tpl');
-$app->layout->_VERSION = 'v0.81';
+$app->layout->_VERSION = 'v1.0.0';
 $app->layout->_MENU = file_get_contents('tpl/menu.tpl');
 
 if (!$app->controller) $app->controller = 'deploy';
